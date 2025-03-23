7da9b6d74bb948b0151ae36b3cee969f7ac95d92
kokolovk123 <161872811+kokolovk123@users.noreply.github.com>
2025-03-23 23:53:27 +0630

    inichain-setup.sh

--- inichain-setup.sh
+++ inichain-setup.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
+
+# Update & install required packages
 sudo apt update && sudo apt upgrade -y
 sudo apt install screen wget curl -y
 
-# Download INIChain Miner
-wget https://github.com/Project-InitVerse/miner/releases/latest/download/inichain-miner
-chmod +x inichain-miner
+# Download INIChain Miner binary
+wget -O iniminer-linux-x64 https://github.com/Project-InitVerse/miner/releases/download/v1.0.0/iniminer-linux-x64
 
-# Start mining
-screen -dmS inichain ./inichain-miner --pool pool-a.yatespool.com:31588 --wallet 0x574FDEA623F4518F950Bd03Ab2add2A30CCBd1Dd
+# Give execute permission
+chmod +x iniminer-linux-x64
+
+# Start Mining
+screen -dmS inichain ./iniminer-linux-x64 --pool pool-a.yatespool.com:31588 --wallet 0x574FDEA623F4518F950Bd03Ab2add2A30CCBd1Dd
